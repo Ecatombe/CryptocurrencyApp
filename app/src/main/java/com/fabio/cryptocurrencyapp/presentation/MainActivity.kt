b6c3686868e503bb2a876c627e628213de2a7d94
Fabio Insolia <fabio.insolia@gmail.com>
2022-08-08 12:56:10 +0100

    Nav component completed

--- app/src/main/java/com/fabio/cryptocurrencyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/fabio/cryptocurrencyapp/presentation/MainActivity.kt
@@ -10,8 +10,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.fabio.cryptocurrencyapp.presentation.coin_detail.CoinDetailScreen
+import com.fabio.cryptocurrencyapp.presentation.coin_list.CoinListScreen
 import com.fabio.cryptocurrencyapp.presentation.ui.theme.CryptocurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(
+                                navController
+                            )
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+
+                    }
                 }
             }
         }
